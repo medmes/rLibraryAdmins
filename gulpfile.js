5d2565b87ce7c8c342c7c3af4f749430748ec06f
MES <mohammed.mesaoudi@gmail.com>
2017-01-17 10:06:19 +0000

    Initial commit & Gulp Configuration.

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect'); //Runs a local dev server
+var open = require('gulp-open'); //Open a URL in a web browser
+
+var config = {
+	port: 9005,
+	devBaseUrl: 'http://localhost',
+	paths: {
+		html: './src/*.html',
+		dist: './dist'
+	}
+};
+
+//Start a local development server
+gulp.task('connect', function() {
+	connect.server({
+		root: ['dist'],
+		port: config.port,
+		base: config.devBaseUrl,
+		livereload: true
+	});
+});
+
+gulp.task('open', ['connect'], function() {
+	gulp.src('dist/index.html')
+		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+// Move Html files to dist folder and reload app
+gulp.task('html', function() {
+	gulp.src(config.paths.html)
+		.pipe(gulp.dest(config.paths.dist))
+		.pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'open']);
