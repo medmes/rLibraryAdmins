7254bb4f7276e5ef9f7cdc48b0f8a6764ad45ee4
MES <mohammed.mesaoudi@gmail.com>
2017-01-19 17:03:40 +0000

    First Component Created

---
+++ src/components/homePage.js
@@ -0,0 +1,1 @@
+"use strict";var React = require('react');var Home = React.createClass({    render: function(){        return(            <div className="jumbotron">                <h1>Welcome to My Library Administration</h1>                <p>React, React Router, Flux for ultra responsive Library web App</p>            </div>        );    }});module.exports = Home;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,1 @@
-$ = jQuery = require('jquery');var App = console.log('Hello World from Browserify');module.exports = App;
+$ = jQuery = require('jquery');var React = require('react');var ReactDOM = require('react-dom');var Home = require('./components/homePage');ReactDOM.render(<Home />, document.getElementById('app'));
